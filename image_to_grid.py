8f12f4f155bc9867daf5d2dbc4d48c3938709339
Antonio f <1115tonyflores@gmail.com>
2026-01-12 15:52:29 -0600

    feat: improve image preprocessing with blur and adaptive thresholding

--- image_to_grid.py
+++ image_to_grid.py
@@ -8,7 +8,7 @@ def image_to_grid(image_path, grid_size):
     Turns an image into a local occupancy grid
     """
 
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(image_path)
 
     if img is None:
         raise ValueError("Image not found")
@@ -16,11 +16,22 @@ def image_to_grid(image_path, grid_size):
     grid_height, grid_width = grid_size
     
     img = cv2.resize(img, (grid_width, grid_height))
-    _, img_bw = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+    bw = cv2.adaptiveThreshold(
+    blur,
+    255,
+    cv2.ADAPTIVE_THRESH_MEAN_C,
+    cv2.THRESH_BINARY,
+    11,
+    2
+)
     local_grid = []
 
-    for row in img_bw:
+    for row in bw:
         grid_row = []
         for pixel in row: 
             if pixel == 0:
